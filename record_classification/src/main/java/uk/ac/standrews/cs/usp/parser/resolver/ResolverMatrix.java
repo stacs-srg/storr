01bc08eae92a893aeb3ef7396712b611ee5eeef3
Jamie Carson <jkc25@st-andrews.ac.uk>
2014-06-16 16:50:43 +0100

    Javadocing ResolverMatrix
    Former-commit-id: 2bdb4dc9ed1f3527c206b5dfdf27306430ec8c23

--- record_classification/src/main/java/uk/ac/standrews/cs/usp/parser/resolver/ResolverMatrix.java
+++ record_classification/src/main/java/uk/ac/standrews/cs/usp/parser/resolver/ResolverMatrix.java
@@ -7,24 +7,48 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import uk.ac.standrews.cs.usp.parser.classifiers.AbstractClassifier;
 import uk.ac.standrews.cs.usp.parser.datastructures.TokenSet;
 import uk.ac.standrews.cs.usp.parser.datastructures.code.Code;
 
 import com.google.common.collect.Multiset;
 
 /**
- *
+ * ResolverMatrix stores mappings between {@link Code}s and the list of {@link Pair}<TokenSet, Double> objects
+ * that were classified as that code.
+ * 
+ * It implements methods that allow  for the reduction in size of the lists for each code via removing overlapping
+ * codes and low confidence codes.
+ * 
+ * It also implements the getValidCodeTriples() method that allows the user to find all Code->Pair<TokenSet, Double>
+ * mappings that are valid with respect to a given power set.
+ * 
+ * @author frjd2
+ * @author jkc25
  * Created by fraserdunlop on 10/06/2014 at 14:57.
  */
 public class ResolverMatrix {
 
+    /** The Code, List<Pair> matrix. */
     Map<Code, List<Pair<TokenSet, Double>>> matrix;
 
+    /**
+     * Instantiates a new empty resolver matrix.
+     */
     public ResolverMatrix() {
 
         matrix = new HashMap<>();
     }
 
+    /**
+     * Adds a {@link TokenSet} and {@link Pair}<Code, Double> to the matrix.
+     * 
+     * The Pair<Code, Double> should represent the output of an {@link AbstractClassifier} where the Code is the
+     * returned code and the Double represents the confidence of that classification.
+     *
+     * @param tokenSet the tokenSet to add
+     * @param codeDoublePair the Pair<Code, Double> to add
+     */
     public void add(final TokenSet tokenSet, final Pair<Code, Double> codeDoublePair) {
 
         Code code = codeDoublePair.getLeft();
@@ -35,6 +59,15 @@ public class ResolverMatrix {
         matrix.get(code).add(new Pair<>(tokenSet, confidence));
     }
 
+    /**
+     * Gets the valid sets of {@link CodeTriple}s from the matrix in the form of a List.
+     * 
+     * A {@link CodeTriple} is defined as being valid if the union of the Set of TokenSets is a subset of the specified powerSet.
+     * Any null entries in the matrix are not returned.
+     * 
+     * @param powerSet the power set of valid tokenSets
+     * @return List<Set<CodeTriple>> the List of Sets of valid {@link CodeTriple}s
+     */
     public List<Set<CodeTriple>> getValidCodeTriples(final Multiset<TokenSet> powerSet) {
 
         resolveHierarchies();
@@ -47,6 +80,15 @@ public class ResolverMatrix {
         return merged;
     }
 
+    /**
+     * Calculates a numerical value for the complexity of the matrix.
+     * This value is calculated by multiplying the size of each list by the size of every other list.
+     * For example, a Map with 3 different codes, with lists of length 4, 5 and 6 would result in a complexity of
+     * 120 being returned (4*5*6).
+     * 
+     * @return the int numerical representation of the complexity of the matrix.
+     * 
+     */
     public int complexity() {
 
         int complexity = 1;
@@ -56,6 +98,10 @@ public class ResolverMatrix {
         return complexity;
     }
 
+    /**
+     * Resolves hierarchies in the matrix by removing the ancestors of {@link Code}s in the matrix. 
+     * This utilises the ResolverUtils.removeAncestors() method to achieve this. 
+     */
     protected void resolveHierarchies() {
 
         Set<Code> keySet = new HashSet<>();
@@ -68,6 +114,14 @@ public class ResolverMatrix {
         }
     }
 
+    /**
+     * Helper method used to enumerate all values in the matrix.
+     *
+     * @param merged the merged
+     * @param pairs the pairs
+     * @param code the code
+     * @param powerSet the power set
+     */
     private void merge(final List<Set<CodeTriple>> merged, final List<Pair<TokenSet, Double>> pairs, final Code code, final Multiset<TokenSet> powerSet) {
 
         List<Set<CodeTriple>> temporaryMerge = new ArrayList<>();
@@ -87,11 +141,21 @@ public class ResolverMatrix {
         merged.addAll(temporaryMerge);
     }
 
+    /**
+     * Chop until complexity within bound.
+     *
+     * @param bound the bound
+     */
     public void chopUntilComplexityWithinBound(final int bound) {
 
         //TODO chop in some way removing poor confidence pairs to get complexity within bound
     }
 
+    /**
+     * Removes from the matrix any Pair<Code, Double> where the confidence value is lower than the confidence specified.
+     *
+     * @param confidence the confidence threshold
+     */
     public void chopBelowConfidence(final Double confidence) {
 
         for (Code code : matrix.keySet()) {
