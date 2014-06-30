3ecb33a76e572cef52b28470bd96acbd02304273
Jamie Carson <jkc25@st-andrews.ac.uk>
2014-06-30 14:47:47 +0100

    Tidy package names
    Former-commit-id: 034e962cbaf5fb1e97759dc542eb48f90f5f05b2

---
+++ record_classification/src/test/java/uk/ac/standrews/cs/digitising_scotland/record_classification/datastructures/dummies/MockBucketFactoryTest.java
@@ -0,0 +1,30 @@
+package uk.ac.standrews.cs.digitising_scotland.record_classification.datastructures.dummies;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import uk.ac.standrews.cs.digitising_scotland.record_classification.datastructures.Bucket;
+
+/**
+ * Testing mock bucket factory.
+ * Created by fraserdunlop on 19/06/2014 at 10:54.
+ */
+public class MockBucketFactoryTest {
+
+    private MockBucketFactory mockBucketFactory;
+
+    @Before
+    public void setup() {
+
+        mockBucketFactory = new MockBucketFactory();
+    }
+
+    @Test
+    public void dummyBucket_HasCorrectSizeTest() {
+
+        Bucket dummyBucket = mockBucketFactory.generateBucketWithDummyRecords(10);
+        assertEquals(10, dummyBucket.size());
+    }
+}
