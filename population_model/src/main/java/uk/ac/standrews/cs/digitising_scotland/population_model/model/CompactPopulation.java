4bb52f5b0381675f80d27afd3eaed18efbe961ce
Graham Kirby <graham.kirby@st-andrews.ac.uk>
2014-07-03 23:18:15 +0100

    Use binary split for searching people by id.
    Former-commit-id: b5795897b21f72ed239067fdc3f05496f8121d2a

--- population_model/src/main/java/uk/ac/standrews/cs/digitising_scotland/population_model/model/CompactPopulation.java
+++ population_model/src/main/java/uk/ac/standrews/cs/digitising_scotland/population_model/model/CompactPopulation.java
@@ -48,10 +48,7 @@ import java.util.Set;
  */
 public class CompactPopulation {
 
-    // TODO define a general interface to be implemented by this, the db and a GEDCOM reader.
-
     // TODO provide a way to configure the parameters dynamically
-    // TODO consider a different implementation growing from a seed population.
 
     /**
      * The approximate average number of days per year.
@@ -67,7 +64,6 @@ public class CompactPopulation {
      * The end year of the simulation.
      */
     public static final int END_YEAR = 2013;
-    public static final int NUMBER_OF_STAGES_IN_POPULATION_GENERATION = 5;
 
     private static final int DAYS_IN_DECEMBER = 31;
     private static final int DECEMBER_INDEX = 11;
@@ -76,10 +72,6 @@ public class CompactPopulation {
     private static final int AGE_AT_FIRST_MARRIAGE_MEAN = 18;
     private static final int MARRIAGE_SEPARATION_MEAN = 7;
     private static final int MARRIAGE_SEPARATION_STD_DEV = 2;
-    private static final int INTER_CHILD_INTERVAL = 3;
-    private static final int TIME_BEFORE_FIRST_CHILD = 1;
-    private static final int MINIMUM_PERIOD_BETWEEN_PARTNERSHIPS = 7;
-    private static final int PARTNERSHIP_AGE_DIFFERENCE_LIMIT = 5;
     private static final int MAX_CHILDREN = 6;
     private static final int MAX_MARRIAGES = 3;
 
@@ -87,6 +79,8 @@ public class CompactPopulation {
     private static final int[] NUMBER_OF_MARRIAGES_DISTRIBUTION = new int[]{4, 20, 2, 1};
 
     private static final double PROBABILITY_OF_BEING_INCOMER = 0.125;
+
+    private static final int NUMBER_OF_STAGES_IN_POPULATION_GENERATION = 5;
 
     private final int earliest_date;
     private final int latest_date;
@@ -104,21 +98,13 @@ public class CompactPopulation {
     private CompactPerson[] people;
     private int number_of_partnerships = 0;
 
-    // TODO make into enum
-    public interface Condition {
-        int POSITIVE = 1;
-        int NEGATIVE_STOP = 2;
-        int NEGATIVE_CONTINUE = 3;
-
-        int check(int index);
-    }
-
     /**
      * Creates a synthetic population.
      *
-     * @param population_size the number of people in the population
-     * @param earliest_date   the earliest date of any event (birth, death, marriage, parenthood), coded DateConversion format - using e.g. DateConversion.dateToDays(START_YEAR, 0, 1)
-     * @param latest_date     the latest date of any event, coded DateConversion format
+     * @param population_size    the number of people in the population
+     * @param earliest_date      the earliest date of any event (birth, death, marriage, parenthood), represented as days since {@link uk.ac.standrews.cs.digitising_scotland.util.DateManipulation#START_YEAR}
+     * @param latest_date        the latest date of any event, represented as days since {@link uk.ac.standrews.cs.digitising_scotland.util.DateManipulation#START_YEAR}
+     * @param progress_indicator a progress indicator
      */
     public CompactPopulation(final int population_size, final int earliest_date, final int latest_date, final ProgressIndicator progress_indicator) throws NegativeWeightException, NegativeDeviationException {
 
@@ -151,7 +137,8 @@ public class CompactPopulation {
     /**
      * Creates a synthetic population with default start and end dates.
      *
-     * @param population_size the number of people in the population
+     * @param population_size    the number of people in the population
+     * @param progress_indicator a progress indicator
      */
     public CompactPopulation(final int population_size, final ProgressIndicator progress_indicator) throws NegativeDeviationException, NegativeWeightException {
 
@@ -169,7 +156,7 @@ public class CompactPopulation {
     }
 
     @SuppressFBWarnings(value = "EI_EXPOSE_REP", justification = "too expensive...")
-    public CompactPopulation(final CompactPerson[] people, final int earliest_date, final int latest_date) {
+    protected CompactPopulation(final CompactPerson[] people, final int earliest_date, final int latest_date) {
 
         this.people = people;
         this.earliest_date = earliest_date;
@@ -179,9 +166,9 @@ public class CompactPopulation {
     }
 
     /**
-     * Get the size of the population.
+     * Gets the number of people in the population.
      *
-     * @return the size.
+     * @return the number of people in the population
      */
     public int size() {
 
@@ -189,76 +176,45 @@ public class CompactPopulation {
     }
 
     /**
-     * Get the number of males in the population.
+     * Gets the person at the specified position in the population.
      *
-     * @return the number of males.
+     * @param index the index
+     * @return the person at that position in the population
      */
-    public int numberMales() {
-
-        int count = 0;
-
-        for (final CompactPerson p : people) {
-            if (p.isMale()) {
-                count++;
-            }
-        }
-        return count;
-    }
-
-    /**
-     * Get the number of males in the population.
-     *
-     * @return the number of females.
-     */
-    public int numberFemales() {
-
-        int count = 0;
-
-        for (final CompactPerson p : people) {
-            if (!p.isMale()) {
-                count++;
-            }
-        }
-        return count;
-    }
-
-    /**
-     * Get the first date of the population.
-     *
-     * @return the first date.
-     */
-    public int getFirstDate() {
-
-        return earliest_date;
-    }
-
-    /**
-     * Get the last date of the population.
-     *
-     * @return the last date.
-     */
-    public int getLastDate() {
-
-        return latest_date;
-    }
-
     public CompactPerson getPerson(final int index) {
 
         return people[index];
     }
 
-    public CompactPerson findPerson(final int id) {
+    /**
+     * Gets the person with the specified identifier.
+     *
+     * @param id the identifier
+     * @return the person with that identifier, or null if none is found
+     */
+    public CompactPerson findPerson(int id) {
 
-        // TODO use binary split since ids are in ascending order in array
+        // Use binary split since the array is sorted by id order.
+        int low = 0;
+        int high = people.length - 1;
 
-        int index = findPerson(-1, new Condition() {
-            @Override
-            public int check(final int index) {
-                return people[index].getId() == id ? Condition.POSITIVE : Condition.NEGATIVE_CONTINUE;
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            CompactPerson mid_person = people[mid];
+
+            if (id < mid_person.id) {
+                high = mid - 1;
+
+            } else if (id > mid_person.id) {
+                low = mid + 1;
+
+            } else {
+                return mid_person;
             }
-        });
-
-        return index > -1 ? people[index] : null;
+        }
+        return null;
     }
 
     public CompactPartnership findPartnership(final int id) {
@@ -267,7 +223,9 @@ public class CompactPopulation {
             List<CompactPartnership> partnerships = person.getPartnerships();
             if (partnerships != null) {
                 for (CompactPartnership partnership : partnerships) {
-                    if (partnership.getId() == id) return partnership;
+                    if (partnership.getId() == id) {
+                        return partnership;
+                    }
                 }
             }
         }
@@ -276,30 +234,30 @@ public class CompactPopulation {
 
     public int findPersonIndex(final CompactPerson person) {
 
-        final Condition match = new Condition() {
+        final SearchCondition match = new SearchCondition() {
 
             @Override
-            public int check(final int person_index) {
-                return (person == people[person_index]) ? Condition.POSITIVE : Condition.NEGATIVE_CONTINUE;
+            public ConditionResult check(final int person_index) {
+                return (person == people[person_index]) ? ConditionResult.POSITIVE : ConditionResult.NEGATIVE_CONTINUE;
             }
         };
 
         return findPerson(0, match);
     }
 
-    public int findPerson(final int start_index, final Condition condition) {
+    public int findPerson(final int start_index, final SearchCondition condition) {
 
         int population_size = people.length;
         for (int i = start_index + 1; i < population_size; i++) {
 
             switch (condition.check(i)) {
 
-                case Condition.POSITIVE:
+                case POSITIVE:
                     return i;
-                case Condition.NEGATIVE_STOP:
-                    return -1;
-                case Condition.NEGATIVE_CONTINUE:
+                case NEGATIVE_CONTINUE:
                     continue;
+                case NEGATIVE_STOP:
+                    return -1;
                 default:
                     throw new RuntimeException("unexpected condition");
             }
@@ -320,20 +278,6 @@ public class CompactPopulation {
         return false;
     }
 
-    public boolean parentsHaveSensibleAgesAtChildBirth(final int father_index, final int mother_index, final int child_index) {
-
-        final CompactPerson father = people[father_index];
-        final CompactPerson mother = people[mother_index];
-        final CompactPerson child = people[child_index];
-
-        return parentsHaveSensibleAgesAtChildBirth(father, mother, child);
-    }
-
-    public static boolean parentsHaveSensibleAgesAtChildBirth(final CompactPerson father, final CompactPerson mother, final CompactPerson child) {
-
-        return PopulationLogic.parentsHaveSensibleAgesAtChildBirth(father.birth_date, father.death_date, mother.birth_date, mother.death_date, child.birth_date);
-    }
-
     public int getNumberOfPeople() {
         return people.length;
     }
@@ -349,9 +293,11 @@ public class CompactPopulation {
     }
 
     /**
-     * Generate people with random birthdays drawn from distribution, then sort into date of birth order.
+     * Generates people with random birthdays drawn from distribution, then sorts into date of birth order.
      */
     private void createPeople() {
+
+        int id = IDFactory.getNextID();
 
         for (int i = 0; i < people.length; i++) {
             people[i] = new CompactPerson(date_of_birth_distribution.getSample(), sex_distribution.getSample());
@@ -359,6 +305,11 @@ public class CompactPopulation {
         }
 
         sortPeopleByAge();
+
+        // Set the person ids in order so binary split can be used to locate ids.
+        for (int i = 0; i < people.length; i++) {
+            people[i].id = id++;
+        }
     }
 
     private void createDeaths() {
@@ -480,7 +431,7 @@ public class CompactPopulation {
 
         for (int i = 0; i < number_of_children; i++) {
 
-            final int earliest_birth_date = earliestAcceptableBirthDate(partnership, previous_child_birth_date);
+            final int earliest_birth_date = PopulationLogic.earliestAcceptableBirthDate(partnership.getMarriageDate(), previous_child_birth_date);
 
             final int child_index = findSuitableChild(partnership, start_index, earliest_birth_date, latest_acceptable_birth_date);
 
@@ -496,11 +447,6 @@ public class CompactPopulation {
             child.setHasParents();
             start_index = child_index + 1; // start looking beyond the last child already chosen.
         }
-    }
-
-    private int earliestAcceptableBirthDate(final CompactPartnership partnership, final int previous_child_birth_date) {
-
-        return previous_child_birth_date == 0 ? DateManipulation.addYears(partnership.getMarriageDate(), TIME_BEFORE_FIRST_CHILD) : DateManipulation.addYears(previous_child_birth_date, INTER_CHILD_INTERVAL);
     }
 
     private void initialiseProgressIndicator() {
@@ -522,17 +468,29 @@ public class CompactPopulation {
         final int husband_index = getHusbandIndex(partnership);
         final int wife_index = getWifeIndex(partnership);
 
-        final Condition conditions = new Condition() {
+        final SearchCondition conditions = new SearchCondition() {
 
             @Override
-            public int check(final int child_index) {
+            public ConditionResult check(final int child_index) {
 
                 final CompactPerson p = people[child_index];
 
-                if (p.birth_date >= latest_birth_date)
-                    return Condition.NEGATIVE_STOP;
+                if (p.birth_date >= latest_birth_date) {
+                    return ConditionResult.NEGATIVE_STOP;
+                }
 
-                return !p.isIncomer() && p.birth_date > earliest_birth_date && p.birth_date < latest_birth_date && !p.hasParents() && parentsHaveSensibleAgesAtChildBirth(husband_index, wife_index, child_index) && !marriedToAnyChildrenOf(child_index, partnership) ? Condition.POSITIVE : Condition.NEGATIVE_CONTINUE;
+                final CompactPerson father = people[husband_index];
+                final CompactPerson mother = people[wife_index];
+                final CompactPerson child = people[child_index];
+
+                return !p.isIncomer() &&
+                        p.birth_date > earliest_birth_date &&
+                        p.birth_date < latest_birth_date &&
+                        !p.hasParents() &&
+                        PopulationLogic.parentsHaveSensibleAgesAtChildBirth(father.birth_date, father.death_date, mother.birth_date, mother.death_date, child.birth_date) &&
+                        !marriedToAnyChildrenOf(child_index, partnership) ?
+
+                        ConditionResult.POSITIVE : ConditionResult.NEGATIVE_CONTINUE;
             }
         };
 
@@ -565,12 +523,12 @@ public class CompactPopulation {
      */
     private int findPartner(final int first_partner_index, final int start_index, final int marriage_date) {
 
-        final Condition partner_compatible = new Condition() {
+        final SearchCondition partner_compatible = new SearchCondition() {
 
             @Override
-            public int check(final int person_index) {
+            public ConditionResult check(final int person_index) {
 
-                return partnersAreCompatible(first_partner_index, person_index, marriage_date) ? Condition.POSITIVE : Condition.NEGATIVE_CONTINUE;
+                return partnersAreCompatible(first_partner_index, person_index, marriage_date) ? ConditionResult.POSITIVE : ConditionResult.NEGATIVE_CONTINUE;
             }
         };
 
@@ -582,12 +540,11 @@ public class CompactPopulation {
         final CompactPerson person = people[partner_index];
         final CompactPerson candidate_partner = people[candidate_partner_index];
 
-        return CompactPerson.oppositeSex(person, candidate_partner) && ageDifferenceNotTooGreat(person, candidate_partner) && notPreviouslyPartners(person, partner_index, candidate_partner) && notTooManyPartnerships(candidate_partner) && notTooRecentPartnership(candidate_partner, marriage_date);
-    }
-
-    private boolean ageDifferenceNotTooGreat(final CompactPerson person, final CompactPerson candidate_partner) {
-
-        return Math.abs(DateManipulation.differenceInYears(person.birth_date, candidate_partner.birth_date)) <= PARTNERSHIP_AGE_DIFFERENCE_LIMIT;
+        return CompactPerson.oppositeSex(person, candidate_partner) &&
+                PopulationLogic.partnerAgeDifferenceIsReasonable(person.birth_date, candidate_partner.birth_date) &&
+                notPreviouslyPartners(person, partner_index, candidate_partner) &&
+                notTooManyPartnerships(candidate_partner) &&
+                notTooRecentPartnership(candidate_partner, marriage_date);
     }
 
     private boolean notPreviouslyPartners(final CompactPerson person, final int index, final CompactPerson candidate_partner) {
@@ -612,8 +569,15 @@ public class CompactPopulation {
 
     private boolean notTooRecentPartnership(final CompactPerson candidate_partner, final int marriage_date) {
 
-        return candidate_partner.getPartnerships() == null || DateManipulation.differenceInYears(candidate_partner.mostRecentPartnership().getMarriageDate(), marriage_date) > MINIMUM_PERIOD_BETWEEN_PARTNERSHIPS;
+        CompactPartnership recent_partnership = candidate_partner.mostRecentPartnership();
+        if (recent_partnership == null) {
+            return false;
+        }
+
+        int most_recent_marriage_date = recent_partnership.getMarriageDate();
+        return PopulationLogic.longEnoughBetweenMarriages(marriage_date, most_recent_marriage_date);
     }
+
 
     private void sortPeopleByAge() {
 
