9d71bd2c377164554207f946ff62e2de083882e7
Graham Kirby <graham.kirby@st-andrews.ac.uk>
2014-07-13 22:34:07 +0100

    Updated licence.
    Former-commit-id: 1bea146fadbc99c3a9451a766e5d91ec1486f03c

--- util/src/main/java/uk/ac/standrews/cs/digitising_scotland/util/CountingConnection.java
+++ util/src/main/java/uk/ac/standrews/cs/digitising_scotland/util/CountingConnection.java
@@ -2,16 +2,16 @@
  * Copyright 2014 Digitising Scotland project:
  * <http://digitisingscotland.cs.st-andrews.ac.uk/>
  *
- * This file is part of the module population_model.
+ * This file is part of the module util.
  *
- * population_model is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * util is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
  * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
  * version.
  *
- * population_model is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * util is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
  * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with population_model. If not, see
+ * You should have received a copy of the GNU General Public License along with util. If not, see
  * <http://www.gnu.org/licenses/>.
  */
 package uk.ac.standrews.cs.digitising_scotland.util;
