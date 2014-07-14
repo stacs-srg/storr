292c117de425a070dbb8e62e22ed2b71989c3d8e
Jamie Carson <jkc25@st-andrews.ac.uk>
2014-07-14 11:46:54 +0100

    Refactor package structures
    Former-commit-id: e0ec5fe354c625e1016346720b2e66cb13e22a3a

--- record_classification/src/main/java/uk/ac/standrews/cs/digitising_scotland/record_classification/datastructures/tokens/package-info.java
+++ record_classification/src/main/java/uk/ac/standrews/cs/digitising_scotland/record_classification/datastructures/tokens/package-info.java
@@ -1,8 +1,7 @@
 /**
- * 
- */
-/**
+ * Package contains classes related to Tokens, TokenSets, TokenCaches etc.
  * @author jkc25
  *
  */
 package uk.ac.standrews.cs.digitising_scotland.record_classification.datastructures.tokens;
+
