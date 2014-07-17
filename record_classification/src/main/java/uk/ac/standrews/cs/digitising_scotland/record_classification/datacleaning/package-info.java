8bfeb5dce5aef394f954558ff7f2a3a1789fae90
Jamie Carson <jkc25@st-andrews.ac.uk>
2014-07-17 15:12:03 +0100

    Add package-info for dataCleaning package
    Former-commit-id: c277c891ea99855a8007de1e499282009f33d295

---
+++ record_classification/src/main/java/uk/ac/standrews/cs/digitising_scotland/record_classification/datacleaning/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Classes that can can perform data cleaning such as spelling correction of feature selection.
+ * @author jkc25, frjd2
+ *
+ */
+package uk.ac.standrews.cs.digitising_scotland.record_classification.datacleaning;
+
