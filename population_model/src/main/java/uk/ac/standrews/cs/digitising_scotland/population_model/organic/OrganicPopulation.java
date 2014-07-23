7299ea4722c17d242d04bc3e55c4173001f3196c
va9 <devnull@localhost>
2014-07-23 16:32:32 +0100

    Trying to solve build synchronization issue between local and remote. Also, merge.
    Former-commit-id: 194effb1740ba900d12c27e9afd205d16a9d1b33

--- population_model/src/main/java/uk/ac/standrews/cs/digitising_scotland/population_model/organic/OrganicPopulation.java
+++ population_model/src/main/java/uk/ac/standrews/cs/digitising_scotland/population_model/organic/OrganicPopulation.java
@@ -16,22 +16,14 @@
  */
 package uk.ac.standrews.cs.digitising_scotland.population_model.organic;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Random;
-
 import uk.ac.standrews.cs.digitising_scotland.population_model.distributions.AgeAtDeathDistribution;
 import uk.ac.standrews.cs.digitising_scotland.population_model.distributions.Distribution;
 import uk.ac.standrews.cs.digitising_scotland.population_model.distributions.RemarriageDistribution;
-import uk.ac.standrews.cs.digitising_scotland.population_model.model.IDFactory;
-import uk.ac.standrews.cs.digitising_scotland.population_model.model.IPartnership;
-import uk.ac.standrews.cs.digitising_scotland.population_model.model.IPerson;
-import uk.ac.standrews.cs.digitising_scotland.population_model.model.IPopulation;
-import uk.ac.standrews.cs.digitising_scotland.population_model.model.PopulationLogic;
+import uk.ac.standrews.cs.digitising_scotland.population_model.model.*;
 import uk.ac.standrews.cs.digitising_scotland.population_model.util.RandomFactory;
 import uk.ac.standrews.cs.digitising_scotland.util.DateManipulation;
+
+import java.util.*;
 
 /**
  * Created by victor on 11/06/14.
