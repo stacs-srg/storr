dac6fed59b49778d176ed8280a531ff82cd0583c
Graham Kirby <graham.kirby@st-andrews.ac.uk>
2014-07-24 14:18:31 +0100

    Refactoring.
    Former-commit-id: 3247ad53ecf9eb1ff4f43cba54420809f2903f03

---
+++ population_model/src/main/java/uk/ac/standrews/cs/digitising_scotland/population_model/model/in_memory/CompactPopulationChildLinker.java
@@ -0,0 +1,219 @@
+/**
+ * Copyright 2014 Digitising Scotland project:
+ * <http://digitisingscotland.cs.st-andrews.ac.uk/>
+ *
+ * This file is part of the module population_model.
+ *
+ * population_model is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * population_model is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with population_model. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+package uk.ac.standrews.cs.digitising_scotland.population_model.model.in_memory;
+
+import uk.ac.standrews.cs.digitising_scotland.population_model.distributions.NegativeWeightException;
+import uk.ac.standrews.cs.digitising_scotland.population_model.distributions.WeightedIntegerDistribution;
+import uk.ac.standrews.cs.digitising_scotland.population_model.model.PopulationLogic;
+import uk.ac.standrews.cs.digitising_scotland.population_model.model.RandomFactory;
+import uk.ac.standrews.cs.digitising_scotland.population_model.model.SearchCondition;
+import uk.ac.standrews.cs.digitising_scotland.util.ProgressIndicator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by graham on 24/07/2014.
+ */
+public class CompactPopulationChildLinker {
+
+    private static final int MAX_CHILDREN = 6;
+    private static final int[] NUMBER_OF_CHILDREN_DISTRIBUTION = new int[]{2, 3, 2, 1, 1, 1, 1};
+
+    private final CompactPerson[] people;
+    private final ProgressIndicator progress_indicator;
+
+    private final WeightedIntegerDistribution number_of_children_distribution;
+
+    public CompactPopulationChildLinker(final CompactPerson[] people, final ProgressIndicator progress_indicator) throws NegativeWeightException {
+
+        this.people = people;
+        this.progress_indicator = progress_indicator;
+
+        number_of_children_distribution = new WeightedIntegerDistribution(0, MAX_CHILDREN, NUMBER_OF_CHILDREN_DISTRIBUTION, RandomFactory.getRandom());
+    }
+
+    public void linkChildren() {
+
+        for (final CompactPerson person : people) {
+
+            linkChildren(person);
+            progressStep();
+        }
+    }
+
+    private void linkChildren(final CompactPerson person) {
+
+        final List<CompactPartnership> partnerships = person.getPartnerships();
+        if (partnerships != null) {
+
+            for (int i = 0; i < partnerships.size(); i++) {
+                final int start_of_next_partnership = i < partnerships.size() - 1 ? partnerships.get(i + 1).getMarriageDate() : Integer.MAX_VALUE;
+                linkChildren(partnerships.get(i), start_of_next_partnership);
+            }
+        }
+    }
+
+    private void linkChildren(final CompactPartnership partnership, final int start_of_next_partnership) {
+
+        // Ignore if child_ids have already been linked for this partnership.
+        if (partnership.getChildren() == null) {
+            final int number_of_children = number_of_children_distribution.getSample();
+            linkChildren(partnership, number_of_children, start_of_next_partnership);
+        }
+    }
+
+    @SuppressWarnings("FeatureEnvy")
+    private void linkChildren(final CompactPartnership partnership, final int number_of_children, final int latest_acceptable_birth_date) {
+
+        final List<Integer> children = new ArrayList<>(number_of_children);
+        partnership.setChildren(children);
+
+        int previous_child_birth_date = 0;
+
+        int start_index = Math.max(partnership.getPartner1(), partnership.getPartner2()) + 1; // the index at which we start to search for possible child_ids.
+
+        for (int i = 0; i < number_of_children; i++) {
+
+            final int earliest_birth_date = PopulationLogic.earliestAcceptableBirthDate(partnership.getMarriageDate(), previous_child_birth_date);
+
+            final int child_index = findSuitableChild(partnership, start_index, earliest_birth_date, latest_acceptable_birth_date);
+
+            // May not be able to find suitable child if time has run out due to a subsequent marriage, or the whole simulation ending.
+            if (child_index == -1) {
+                break;
+            }
+
+            final CompactPerson child = people[child_index];
+
+            children.add(child_index);
+            previous_child_birth_date = child.birth_date;
+            child.setHasParents();
+            start_index = child_index + 1; // start looking beyond the last child already chosen.
+        }
+    }
+
+    @SuppressWarnings("FeatureEnvy")
+    private int findSuitableChild(final CompactPartnership partnership, final int start_search_index, final int earliest_birth_date, final int latest_birth_date) {
+
+        final int husband_index = getHusbandIndex(partnership);
+        final int wife_index = getWifeIndex(partnership);
+
+        final SearchCondition conditions = new SearchCondition() {
+
+            @Override
+            public ConditionResult check(final int child_index) {
+
+                final CompactPerson child = people[child_index];
+
+                if (child.birth_date >= latest_birth_date) {
+                    return ConditionResult.NEGATIVE_STOP;
+                }
+
+                final CompactPerson father = people[husband_index];
+                final CompactPerson mother = people[wife_index];
+
+                final boolean not_incomer = !child.isIncomer();
+                final boolean not_born_too_early = child.birth_date > earliest_birth_date;
+                final boolean not_born_too_late = child.birth_date < latest_birth_date;
+                final boolean does_not_have_parents = !child.hasParents();
+                final boolean parents_have_sensible_ages = PopulationLogic.parentsHaveSensibleAgesAtChildBirth(father.birth_date, father.death_date, mother.birth_date, mother.death_date, child.birth_date);
+                final boolean child_not_married_to_existing_child = !marriedToAnyChildrenOf(child_index, partnership);
+
+                return not_incomer &&
+                        not_born_too_early &&
+                        not_born_too_late &&
+                        does_not_have_parents &&
+                        parents_have_sensible_ages &&
+                        child_not_married_to_existing_child ?
+
+                        ConditionResult.POSITIVE : ConditionResult.NEGATIVE_CONTINUE;
+            }
+
+            private boolean marriedToAnyChildrenOf(final int person_index, final CompactPartnership partnership) {
+
+                // Iterate over all partnerships of the people in the given partnership.
+                for (final int partner : new Integer[]{partnership.getPartner1(), partnership.getPartner2()}) {
+                    for (final CompactPartnership partnership2 : people[partner].getPartnerships()) {
+
+                        final List<Integer> children = partnership2.getChildren();
+                        if (children != null) {
+                            for (final int child : children) {
+                                if (married(child, person_index)) {
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+                }
+                return false;
+            }
+
+            private boolean married(final int p1_index, final int p2_index) {
+
+                final List<CompactPartnership> partnerships = people[p1_index].getPartnerships();
+                if (partnerships != null) {
+                    for (final CompactPartnership partnership : partnerships) {
+                        if (partnership.getPartner(p1_index) == p2_index) {
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+        };
+
+        return findPerson(start_search_index, conditions);
+    }
+
+    private int findPerson(final int start_index, final SearchCondition condition) {
+
+        final int population_size = people.length;
+        for (int i = start_index + 1; i < population_size; i++) {
+
+            switch (condition.check(i)) {
+
+                case POSITIVE:
+                    return i;
+                case NEGATIVE_CONTINUE:
+                    continue;
+                case NEGATIVE_STOP:
+                    return -1;
+            }
+        }
+        return -1;
+    }
+
+    private int getHusbandIndex(final CompactPartnership partnership) {
+
+        final int partner1_index = partnership.getPartner1();
+        return people[partner1_index].isMale() ? partner1_index : partnership.getPartner2();
+    }
+
+    private int getWifeIndex(final CompactPartnership partnership) {
+
+        final int partner1_index = partnership.getPartner1();
+        return !people[partner1_index].isMale() ? partner1_index : partnership.getPartner2();
+    }
+
+    private void progressStep() {
+
+        if (progress_indicator != null) {
+            progress_indicator.progressStep();
+        }
+    }
+}
---
+++ population_model/src/main/java/uk/ac/standrews/cs/digitising_scotland/population_model/model/in_memory/CompactPopulationPartnerLinker.java
@@ -0,0 +1,217 @@
+/**
+ * Copyright 2014 Digitising Scotland project:
+ * <http://digitisingscotland.cs.st-andrews.ac.uk/>
+ *
+ * This file is part of the module population_model.
+ *
+ * population_model is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * population_model is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with population_model. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+package uk.ac.standrews.cs.digitising_scotland.population_model.model.in_memory;
+
+import uk.ac.standrews.cs.digitising_scotland.population_model.distributions.NegativeDeviationException;
+import uk.ac.standrews.cs.digitising_scotland.population_model.distributions.NegativeWeightException;
+import uk.ac.standrews.cs.digitising_scotland.population_model.distributions.NormalDistribution;
+import uk.ac.standrews.cs.digitising_scotland.population_model.distributions.WeightedIntegerDistribution;
+import uk.ac.standrews.cs.digitising_scotland.population_model.model.PopulationLogic;
+import uk.ac.standrews.cs.digitising_scotland.population_model.model.RandomFactory;
+import uk.ac.standrews.cs.digitising_scotland.population_model.model.SearchCondition;
+import uk.ac.standrews.cs.digitising_scotland.util.DateManipulation;
+import uk.ac.standrews.cs.digitising_scotland.util.ProgressIndicator;
+
+import java.util.Random;
+
+/**
+ * Created by graham on 24/07/2014.
+ */
+public class CompactPopulationPartnerLinker {
+
+    /**
+     * The approximate average number of days per year.
+     */
+    public static final float DAYS_PER_YEAR = 365.25f;
+
+    /**
+     * The end year of the simulation.
+     */
+    public static final int END_YEAR = 2013;
+
+
+    private static final int AGE_AT_FIRST_MARRIAGE_STD_DEV = 3;
+    private static final int AGE_AT_FIRST_MARRIAGE_MEAN = 18;
+    private static final int MARRIAGE_SEPARATION_MEAN = 7;
+    private static final int MARRIAGE_SEPARATION_STD_DEV = 2;
+    private static final int MAX_MARRIAGES = 3;
+
+    @SuppressWarnings("MagicNumber")
+    private static final int[] NUMBER_OF_MARRIAGES_DISTRIBUTION = new int[]{4, 20, 2, 1};
+
+    private final CompactPerson[] people;
+    private final ProgressIndicator progress_indicator;
+
+    private final WeightedIntegerDistribution number_of_marriages_distribution;
+    private final NormalDistribution age_at_first_marriage_distribution;
+    private final NormalDistribution marriage_separation_distribution;
+
+    private int number_of_partnerships = 0;
+
+    public CompactPopulationPartnerLinker(final CompactPerson[] people, final ProgressIndicator progress_indicator) throws NegativeWeightException, NegativeDeviationException {
+
+        this.people = people;
+        this.progress_indicator = progress_indicator;
+
+        final Random random = RandomFactory.getRandom();
+
+        number_of_marriages_distribution = new WeightedIntegerDistribution(0, MAX_MARRIAGES, NUMBER_OF_MARRIAGES_DISTRIBUTION, random);
+        age_at_first_marriage_distribution = new NormalDistribution(AGE_AT_FIRST_MARRIAGE_MEAN * DAYS_PER_YEAR, AGE_AT_FIRST_MARRIAGE_STD_DEV * DAYS_PER_YEAR, random);
+        marriage_separation_distribution = new NormalDistribution(MARRIAGE_SEPARATION_MEAN * DAYS_PER_YEAR, MARRIAGE_SEPARATION_STD_DEV * DAYS_PER_YEAR, random);
+    }
+
+    public int linkPartners() {
+
+        for (int i = 0; i < people.length; i++) {
+
+            if (people[i].getPartnerships() == null) { // Skip if marriages have already been set for this person.
+
+                createMarriages(i, number_of_marriages_distribution.getSample());
+            }
+            progressStep();
+        }
+
+        return number_of_partnerships;
+    }
+
+    private void createMarriages(final int index, final int number_of_marriages) {
+
+        final CompactPerson person = people[index];
+
+        final int death_date = person.getDeathDate();
+        int marriage_date = person.birth_date + (int) (double) age_at_first_marriage_distribution.getSample();
+
+        int start_index = index + 1;
+
+        for (int i = 0; i < number_of_marriages; i++) {
+
+            if (marriage_date < death_date) {
+                final int spouse_index = createMarriage(index, start_index, marriage_date);
+                if (spouse_index == -1) { // can't find a spouse
+                    break;
+                }
+                start_index = spouse_index + 1;
+                marriage_date += marriage_separation_distribution.getSample();
+            }
+        }
+    }
+
+    private int createMarriage(final int first_partner_index, final int start_index, final int marriage_date) {
+
+        if (DateManipulation.daysToYear(marriage_date) <= END_YEAR) {
+            final int partner_index = findPartner(first_partner_index, start_index, marriage_date);
+
+            if (partner_index != -1) {
+                new CompactPartnership(people[first_partner_index], first_partner_index, people[partner_index], partner_index, marriage_date);
+                number_of_partnerships++;
+            }
+
+            return partner_index;
+        }
+        return -1;
+    }
+    /**
+     * Linearly search up people starting at @param index looking for a partner who is sufficiently close in age to the first person and who is either unmarried or not married recently.
+     *
+     * @return the index if a person is found and -1 otherwise.
+     */
+    @SuppressWarnings("FeatureEnvy")
+    private int findPartner(final int first_partner_index, final int start_index, final int marriage_date) {
+
+        final SearchCondition partner_compatible = new SearchCondition() {
+
+            @Override
+            public ConditionResult check(final int person_index) {
+
+                final CompactPerson person = people[first_partner_index];
+                final CompactPerson candidate_partner = people[person_index];
+
+                final boolean opposite_sex = person.getSex() != candidate_partner.getSex();
+                final boolean age_difference_is_reasonable = PopulationLogic.partnerAgeDifferenceIsReasonable(person.birth_date, candidate_partner.birth_date);
+                final boolean not_previously_partners = notPreviouslyPartners(person, first_partner_index, candidate_partner);
+                final boolean not_too_many_partnerships = notTooManyPartnerships(candidate_partner);
+                final boolean not_too_recent_partnership = notTooRecentPartnership(candidate_partner, marriage_date);
+                final boolean still_alive = marriage_date < candidate_partner.getDeathDate();
+
+                return opposite_sex &&
+                        age_difference_is_reasonable &&
+                        not_previously_partners &&
+                        not_too_many_partnerships &&
+                        not_too_recent_partnership &&
+                        still_alive ? ConditionResult.POSITIVE : ConditionResult.NEGATIVE_CONTINUE;
+            }
+
+            private boolean notPreviouslyPartners(final CompactPerson person, final int index, final CompactPerson candidate_partner) {
+
+                if (person.getPartnerships() == null || candidate_partner.getPartnerships() == null) {
+                    return true;
+                }
+
+                for (final CompactPartnership partnership : person.getPartnerships()) {
+                    if (people[partnership.getPartner(index)] == candidate_partner) {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            private boolean notTooManyPartnerships(final CompactPerson candidate_partner) {
+
+                return candidate_partner.getPartnerships() == null || candidate_partner.getPartnerships().size() < MAX_MARRIAGES;
+            }
+
+            private boolean notTooRecentPartnership(final CompactPerson candidate_partner, final int marriage_date) {
+
+                final CompactPartnership recent_partnership = candidate_partner.mostRecentPartnership();
+                if (recent_partnership == null) {
+                    return true;
+                }
+
+                final int most_recent_marriage_date = recent_partnership.getMarriageDate();
+                return PopulationLogic.longEnoughBetweenMarriages(marriage_date, most_recent_marriage_date);
+            }
+        };
+
+        return findPerson(start_index, partner_compatible);
+    }
+
+    private int findPerson(final int start_index, final SearchCondition condition) {
+
+        final int population_size = people.length;
+        for (int i = start_index + 1; i < population_size; i++) {
+
+            switch (condition.check(i)) {
+
+                case POSITIVE:
+                    return i;
+                case NEGATIVE_CONTINUE:
+                    continue;
+                case NEGATIVE_STOP:
+                    return -1;
+            }
+        }
+        return -1;
+    }
+
+    private void progressStep() {
+
+        if (progress_indicator != null) {
+            progress_indicator.progressStep();
+        }
+    }
+}
