ca491b9bfbd7f5c43438cb212bb6201d0cdc3da3
Graham Kirby <graham.kirby@st-andrews.ac.uk>
2014-08-06 19:58:19 +0100

    Fixed licence headers.
    Former-commit-id: 91b30572e8d9ca92a60a7434088d400227fb285e

--- population_model/src/main/java/uk/ac/standrews/cs/digitising_scotland/population_model/analytic/package-info.java
+++ population_model/src/main/java/uk/ac/standrews/cs/digitising_scotland/population_model/analytic/package-info.java
@@ -13,6 +13,8 @@
  *
  * You should have received a copy of the GNU General Public License along with population_model. If not, see
  * <http://www.gnu.org/licenses/>.
+ */
+/**
  *
  * Analytic classes.
  *
