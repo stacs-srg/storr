01ec5fed94a55a97ed671a4b03d527c15312cb70
fraserdunlop <devnull@localhost>
2014-08-18 12:14:35 +0100

    merge
    Former-commit-id: cc60172131a21bae3aa95b47ed3a693aad0960cb

--- record_classification/src/main/java/uk/ac/standrews/cs/digitising_scotland/record_classification/classifiers/OLR/OLRCrossFold.java
+++ record_classification/src/main/java/uk/ac/standrews/cs/digitising_scotland/record_classification/classifiers/OLR/OLRCrossFold.java
@@ -136,9 +136,9 @@ public class OLRCrossFold {
         for (OLRPool model : models) {
             executorService.submit(model);
         }
-        executorService.shutdown();
         final int timeout = 365;
         executorService.awaitTermination(timeout, TimeUnit.DAYS);
+        executorService.shutdown();
         stopListener.terminateProcess();
         stopService.shutdown();
         prepareClassifier();
