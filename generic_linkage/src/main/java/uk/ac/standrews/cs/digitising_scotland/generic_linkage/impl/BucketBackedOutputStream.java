1beb3be87248215602300c98fc355ee3c672ac0d
Jamie Carson <jkc25@st-andrews.ac.uk>
2014-08-19 15:48:48 +0100

    Added BucketBackedOutputStream place holder class so that the builds compile on Jenkins.
    Former-commit-id: b5ceabd4099b003f96492d203cf7be7ea89a7490

---
+++ generic_linkage/src/main/java/uk/ac/standrews/cs/digitising_scotland/generic_linkage/impl/BucketBackedOutputStream.java
@@ -0,0 +1,21 @@
+package uk.ac.standrews.cs.digitising_scotland.generic_linkage.impl;
+
+import uk.ac.standrews.cs.digitising_scotland.generic_linkage.interfaces.IBucket;
+import uk.ac.standrews.cs.digitising_scotland.generic_linkage.interfaces.ILXP;
+import uk.ac.standrews.cs.digitising_scotland.generic_linkage.interfaces.ILXPOutputStream;
+
+public class BucketBackedOutputStream implements ILXPOutputStream {
+
+    public BucketBackedOutputStream(IBucket b2) {
+
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void add(ILXP record) {
+
+        // TODO Auto-generated method stub
+
+    }
+
+}
