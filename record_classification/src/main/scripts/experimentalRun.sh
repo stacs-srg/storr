bb4bdb60159827f96f2ae43798c6101f182d9c3d
jamie carson <jkc25>
2014-08-25 14:59:18 +0100

    Add script for running an experimental run with one file used for training and testing
    Former-commit-id: 2a9747ae8e0e361fb85569e1a5ac7232c46766c3

---
+++ record_classification/src/main/scripts/experimentalRun.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+# This script will install the record_classification software using 'mvn clean compile' then perform a training/classification run
+# using the TrainClassifyOneFile main class. This assumes modern training data that is used for both training and classifying.
+
+# Setup variables
+HEAP="8g"
+USAGE="Usage: $0 <goldStandard File>	<trainingRatio>	<heap size - optional>"
+
+# Check that there is at least 1 argument
+
+[ $# -le 0 ] && { 
+	echo "Invalid number of arguments. Expected between 1 and 3, got $#"; 
+	echo $USAGE; 
+	exit 1; 
+}
+
+# Check that are no more than 3 arguments
+[ $# -ge 4 ] && { 
+	echo "Invalid number of arguments. Expected between 1 and 3, got $#"; 
+	echo $USAGE; 
+	exit 1;  
+}
+
+# Confirm user inputs
+echo "Gold Standard file $1"
+
+if [ -n "$2" ];
+then
+	echo "Training ratio size $2" 
+fi
+
+if [ -n "$3" ];
+then
+	echo "Heap size $3g" 
+	HEAP="$3g"
+fi
+
+
+
+# Build and run model
+mvn clean compile assembly:single
+
+echo startTime: 
+date +"%H:%M:%S"
+
+java -d64 -Xmx$HEAP -cp target/record_classification-1.0-SNAPSHOT-jar-with-dependencies.jar uk.ac.standrews.cs.digitising_scotland.record_classification.pipeline.TrainClassifyOneFile $1 $2
+
+echo finishTime: 
+date +"%H:%M:%S"
