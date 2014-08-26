069e13866ede8296cbb4e6a14798179e60190eb8
jamie carson <jkc25>
2014-08-26 17:13:08 +0100

    Add file to test classification on existing model
    Former-commit-id: 9ea9d1dbafb44cb07a4811fd28c3b9c81dcd10ef

---
+++ record_classification/src/test/java/uk/ac/standrews/cs/digitising_scotland/record_classification/pipeline/ClassifyWithExsistingModelsTest.java
@@ -0,0 +1,16 @@
+package uk.ac.standrews.cs.digitising_scotland.record_classification.pipeline;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class ClassifyWithExsistingModelsTest {
+
+    @Ignore("Implement this test")
+    @Test
+    public void test() throws Exception {
+
+        String[] args = {"icd500cleaned", "target"};
+        ClassifyWithExsistingModels.main(args);
+
+    }
+}
