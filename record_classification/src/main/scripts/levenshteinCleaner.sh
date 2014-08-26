59c36b36a5406fbb81796d5351a19e063ec3af44
jamie carson <jkc25>
2014-08-26 14:15:10 +0100

    Input checking on LevenshteinCleaner script
    Former-commit-id: 070d9286f58d93be1313dddd41f352481c7bef7f

--- record_classification/src/main/scripts/levenshteinCleaner.sh
+++ record_classification/src/main/scripts/levenshteinCleaner.sh
@@ -3,24 +3,47 @@
 # This script will read the arguments supplied to the script and pass them to the LevenshteinCleaner class in the record_classification module.
 
 # Check user inputs and echo to user.
-if [ -n "$1" ];
+
+# Setup environmental and local variables
+set -o pipefail
+set -o errexit
+
+USAGE="Usage: $0 <inputFile>	<outputFile>	<tokenLimit>	<similarityLimit>
+	   tokenLimit and similarity must be integers"
+
+if [ -n "$1" ] ;
 then
-    echo Running data cleaning on $1
+    echo "Running data cleaning on $1"
+else
+	echo "$USAGE"
+	exit 1;
 fi
 
-if [ -n "$2" ];
+if [ -n "$2" ] ;
 then
 	echo Cleaning file will be saved to $2
+else
+	echo "$USAGE"
+	exit 1;
 fi
 
-if [ -n "$3" ];
+
+if [ -n "$3" ] &&  [ "$3" -ne 0 -o "$3" -eq 0 2>/dev/null ];
 then
 	echo Token limit set to $3
+else
+	echo "Supplied Input $3 is not an Integer."
+	echo "$USAGE"
+	exit 1;
 fi
 	
-if [ -n "$4" ];
+if [ -n "$4" ] &&  [ "$4" -ne 0 -o "$4" -eq 0 2>/dev/null ];
 then
  	echo Similarity set to $4
+else
+	echo "Supplied Input $4 is not an Integer."
+	echo "$USAGE"
+	exit 1;
 fi
 
 # install the software with a clean target directory.
