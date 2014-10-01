88dd8833beb21874ca1fd945e511dfe01850aaf2
jamie carson <jkc25>
2014-10-01 17:49:07 +0100

    Fix bug where collections wasn't implementing sort properly.... or something.
    Former-commit-id: 738eb67e756ab135d63a5cb7400494fe560ec9d0

--- record_classification/src/main/java/uk/ac/standrews/cs/digitising_scotland/record_classification/classifiers/lookup/ClosestMatchMap.java
+++ record_classification/src/main/java/uk/ac/standrews/cs/digitising_scotland/record_classification/classifiers/lookup/ClosestMatchMap.java
@@ -1,6 +1,11 @@
 package uk.ac.standrews.cs.digitising_scotland.record_classification.classifiers.lookup;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * TODO test
@@ -9,12 +14,13 @@ import java.util.*;
  * similarity metric supplied at construction.
  * Created by fraserdunlop on 01/10/2014 at 15:38.
  */
-public class ClosestMatchMap<K,V> implements Map<K,V>{
+public class ClosestMatchMap<K, V> implements Map<K, V> {
 
     private final Similaritor<K> similaritor;
-    private Map<K,V> map;
+    private Map<K, V> map;
 
-    ClosestMatchMap(SimilarityMetric<K> metric, Map<K,V> map){
+    ClosestMatchMap(final SimilarityMetric<K> metric, final Map<K, V> map) {
+
         this.map = map;
         this.similaritor = new Similaritor<>(metric);
     }
@@ -27,9 +33,9 @@ public class ClosestMatchMap<K,V> implements Map<K,V>{
      * @param key the key to fetch.
      * @return the value of the key which matches the supplied key most closely.
      */
-    public V getClosestMatch(K key){
-        if(containsKey(key))
-            return get(key);
+    public V getClosestMatch(final K key) {
+
+        if (containsKey(key)) { return get(key); }
         return get(getClosestKey(key));
     }
 
@@ -38,69 +44,82 @@ public class ClosestMatchMap<K,V> implements Map<K,V>{
      * @param key used to construct comparator to sort keys.
      * @return the key at the head of the sorted list of keys.
      */
-    private K getClosestKey(K key) {
+    private K getClosestKey(final K key) {
+
         List<K> keyList = new ArrayList<>(keySet());
-        keyList.sort(similaritor.getComparator(key));
+        Collections.sort(keyList, similaritor.getComparator(key));
         return keyList.get(0);
     }
 
     @Override
     public int size() {
+
         return map.size();
     }
 
     @Override
     public boolean isEmpty() {
+
         return map.isEmpty();
     }
 
     @Override
-    public boolean containsKey(Object key) {
+    public boolean containsKey(final Object key) {
+
         return map.containsKey(key);
     }
 
     @Override
-    public boolean containsValue(Object value) {
+    public boolean containsValue(final Object value) {
+
         return map.containsValue(value);
     }
 
     @Override
-    public V get(Object key) {
+    public V get(final Object key) {
+
         return map.get(key);
     }
 
     @Override
-    public V put(K key, V value) {
+    public V put(final K key, final V value) {
+
         return map.put(key, value);
     }
 
     @Override
-    public V remove(Object key) {
+    public V remove(final Object key) {
+
         return map.remove(key);
     }
 
     @Override
-    public void putAll(Map<? extends K, ? extends V> m) {
+    public void putAll(final Map<? extends K, ? extends V> m) {
+
         map.putAll(m);
     }
 
     @Override
     public void clear() {
+
         map.clear();
     }
 
     @Override
     public Set<K> keySet() {
+
         return map.keySet();
     }
 
     @Override
     public Collection<V> values() {
+
         return map.values();
     }
 
     @Override
     public Set<Entry<K, V>> entrySet() {
+
         return map.entrySet();
     }
 }
