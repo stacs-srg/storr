20a97fa3e8da656efe774caa86b95996d5e242eb
jamie carson <jkc25>
2014-10-08 13:29:14 +0100

    Build test for PIlot pipeline class. Only small sample currently.
    Former-commit-id: f02d8b26de70ffbfca2f729efe49f1741471d919

---
+++ record_classification/src/test/java/uk/ac/standrews/cs/digitising_scotland/record_classification/pipeline/main/PIlotTest.java
@@ -0,0 +1,85 @@
+package uk.ac.standrews.cs.digitising_scotland.record_classification.pipeline.main;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import uk.ac.standrews.cs.digitising_scotland.record_classification.datastructures.bucket.Bucket;
+import uk.ac.standrews.cs.digitising_scotland.record_classification.datastructures.code.Classification;
+
+public class PIlotTest {
+
+    PIlot pilot;
+
+    @Before
+    public void setUp() throws Exception {
+
+        pilot = new PIlot();
+    }
+
+    @Test
+    public void test() throws Exception {
+
+        Iterator<Classification> it;
+        Set<String> codesinmap;
+        String trainingData = getClass().getResource("/PilotTestTrainingData.txt").getFile();
+        String testData = getClass().getResource("/pilotTest.tsv").getFile();
+        String[] args = {trainingData, testData};
+        Bucket allClassified = pilot.run(args);
+
+        Set<Classification> classifications = allClassified.getRecord(46999).getClassifications();
+        System.out.println(classifications);
+        Assert.assertEquals(3, classifications.size());
+        it = classifications.iterator();
+        codesinmap = getCodesInMap(it);
+        Assert.assertTrue(codesinmap.contains("I340"));
+        Assert.assertTrue(codesinmap.contains("I48"));
+        Assert.assertTrue(codesinmap.contains("I515"));
+
+        classifications = allClassified.getRecord(72408).getClassifications();
+        System.out.println(classifications);
+        Assert.assertEquals(1, classifications.size());
+        it = classifications.iterator();
+        codesinmap = getCodesInMap(it);
+        Assert.assertTrue(codesinmap.contains("I340"));
+
+        classifications = allClassified.getRecord(6804).getClassifications();
+        System.out.println(classifications);
+        Assert.assertEquals(2, classifications.size());
+        it = classifications.iterator();
+        codesinmap = getCodesInMap(it);
+        Assert.assertTrue(codesinmap.contains("I219"));
+        Assert.assertTrue(codesinmap.contains("I515"));
+
+        classifications = allClassified.getRecord(43454).getClassifications();
+        System.out.println(classifications);
+        Assert.assertEquals(1, classifications.size());
+        it = classifications.iterator();
+        codesinmap = getCodesInMap(it);
+        Assert.assertTrue(codesinmap.contains("I219"));
+
+        classifications = allClassified.getRecord(6809).getClassifications();
+        System.out.println(classifications);
+        Assert.assertEquals(2, classifications.size());
+        it = classifications.iterator();
+        codesinmap = getCodesInMap(it);
+        Assert.assertTrue(codesinmap.contains("I219"));
+        Assert.assertTrue(codesinmap.contains("I515"));
+
+    }
+
+    private Set<String> getCodesInMap(Iterator<Classification> it) {
+
+        Set<String> codesinmap = new HashSet<>();
+
+        while (it.hasNext()) {
+            Classification classification = (Classification) it.next();
+            codesinmap.add(classification.getCode().getCodeAsString());
+        }
+        return codesinmap;
+    }
+}
