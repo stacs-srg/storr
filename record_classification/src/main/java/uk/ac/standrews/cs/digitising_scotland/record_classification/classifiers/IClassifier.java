19d4e661518e8a7d2a60d612da0a9c3bb7dcbef9
fraserdunlop <devnull@localhost>
2014-10-09 10:49:34 +0100

    documentation
    Former-commit-id: e8325ac67a4d9ac23db66255cb25cc0ff1c603a5

--- record_classification/src/main/java/uk/ac/standrews/cs/digitising_scotland/record_classification/classifiers/IClassifier.java
+++ record_classification/src/main/java/uk/ac/standrews/cs/digitising_scotland/record_classification/classifiers/IClassifier.java
@@ -3,6 +3,6 @@ package uk.ac.standrews.cs.digitising_scotland.record_classification.classifiers
 //TODO document!
 public interface IClassifier<K, V> {
 
-    public abstract V classify(K k);
+    public abstract V classify(K k) throws Exception;
 
 }
