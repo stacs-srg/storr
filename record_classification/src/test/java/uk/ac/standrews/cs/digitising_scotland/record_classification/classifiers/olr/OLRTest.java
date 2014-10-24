800c81f0d974bf27e815626f99e0829855e2ecda
jamie carson <jkc25>
2014-10-24 18:24:19 +0100

    Finish updating tests to work with new properties file structure.
    Former-commit-id: 18ffe93fac6bb3121c0c7f2de54594b668284f43

--- record_classification/src/test/java/uk/ac/standrews/cs/digitising_scotland/record_classification/classifiers/olr/OLRTest.java
+++ record_classification/src/test/java/uk/ac/standrews/cs/digitising_scotland/record_classification/classifiers/olr/OLRTest.java
@@ -1,5 +1,6 @@
 package uk.ac.standrews.cs.digitising_scotland.record_classification.classifiers.olr;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
@@ -50,10 +51,11 @@ public class OLRTest {
     @Before
     public void setup() {
 
+        MachineLearningConfiguration.loadProperties(new File("src/test/resources/machineLearning.default.properties"));
         vectorFactory = new VectorFactory(); //FIXME
         vectors = createVectors();
-        //  MachineLearningConfiguration.getDefaultProperties().setProperty("numFeatures", "20");
-        //MachineLearningConfiguration.getDefaultProperties().setProperty("numCategories", "20");
+        MachineLearningConfiguration.getDefaultProperties().setProperty("numFeatures", "20");
+        MachineLearningConfiguration.getDefaultProperties().setProperty("numCategories", "20");
 
     }
 
@@ -64,11 +66,13 @@ public class OLRTest {
     @Parameterized.Parameters
     public static Collection<Object[]> getParameters() {
 
+        MachineLearningConfiguration.loadProperties(new File("src/test/resources/machineLearning.default.properties"));
+
         final List<Object[]> parameters = new ArrayList<Object[]>();
 
         Properties properties = new Properties(MachineLearningConfiguration.getDefaultProperties());
         properties.setProperty("numCategories", "2");
-        //  properties.setProperty("numFeatures", "50");
+        properties.setProperty("numFeatures", "20");
         properties.setProperty("perTermLearning", "true");
         properties.setProperty("olrRegularisation", "true");
         parameters.add(new Object[]{new Properties(properties)});
