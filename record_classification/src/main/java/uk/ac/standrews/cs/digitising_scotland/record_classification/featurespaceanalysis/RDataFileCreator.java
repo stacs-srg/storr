4c284b37d568f66a0d7b0dcb01d81d67918f72d9
jamie carson <jkc25>
2014-10-28 16:35:46 +0000

    Fix sorted.sort error
    Former-commit-id: 479abce4e68ba78f62687ac86c6cf2c437392ab4

--- record_classification/src/main/java/uk/ac/standrews/cs/digitising_scotland/record_classification/featurespaceanalysis/RDataFileCreator.java
+++ record_classification/src/main/java/uk/ac/standrews/cs/digitising_scotland/record_classification/featurespaceanalysis/RDataFileCreator.java
@@ -1,42 +1,53 @@
 package uk.ac.standrews.cs.digitising_scotland.record_classification.featurespaceanalysis;
 
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
 import uk.ac.standrews.cs.digitising_scotland.record_classification.datastructures.code.Code;
 import uk.ac.standrews.cs.digitising_scotland.record_classification.datastructures.tokens.TokenSet;
-
-import java.text.DecimalFormat;
-import java.util.*;
 
 /**
  *
  * Created by fraserdunlop on 28/10/2014 at 11:27.
  */
 public class RDataFileCreator {
+
     private final FeatureSpaceAnalyser fsa1;
     private final FeatureSpaceAnalyser fsa2;
-    private int outputLimit =15;
+    private int outputLimit = 15;
 
     public RDataFileCreator(FeatureSpaceAnalyser dataSet1FeatureSpaceAnalyser, FeatureSpaceAnalyser dataSet2FeatureSpaceAnalyser) {
+
         this.fsa1 = dataSet1FeatureSpaceAnalyser;
         this.fsa2 = dataSet2FeatureSpaceAnalyser;
     }
 
     public String make(Code code) {
+
         Set<String> features = new HashSet<>();
-        CodeProfile codeProfile1 = getCodeProfile(fsa1,code,features);
+        CodeProfile codeProfile1 = getCodeProfile(fsa1, code, features);
         CodeProfile codeProfile2 = getCodeProfile(fsa2, code, features);
         StringBuilder sb = new StringBuilder();
         List<String> featureList = new ArrayList<>();
-        if(codeProfile1!=null) {
+        if (codeProfile1 != null) {
             featureList = sortFeatures(codeProfile1, features);
-        }else if(codeProfile2!=null){
+        }
+        else if (codeProfile2 != null) {
             featureList = sortFeatures(codeProfile2, features);
         }
         Iterator<String> iterator = featureList.iterator();
         int i = 0;
-        while(iterator.hasNext() && i < outputLimit){
+        while (iterator.hasNext() && i < outputLimit) {
             i++;
-            sb.append((new TokenSet(iterator.next())).toString().replaceAll("'","")); //Regex replace apostrophes as they mess up R's file reader.
-            if(iterator.hasNext() && i < outputLimit)
+            sb.append((new TokenSet(iterator.next())).toString().replaceAll("'", "")); //Regex replace apostrophes as they mess up R's file reader.
+            if (iterator.hasNext() && i < outputLimit)
                 sb.append(",");
             else sb.append("\n");
         }
@@ -47,45 +58,54 @@ public class RDataFileCreator {
     }
 
     private List<String> sortFeatures(CodeProfile codeProfile, Collection<String> features) {
+
         List<String> sorted = new ArrayList<>(features);
-        sorted.sort(new featureComparator(codeProfile));
+        //sorted.sort();
+        Collections.sort(sorted, new featureComparator(codeProfile));
         return sorted;
     }
 
-    private class featureComparator implements Comparator<String>{
+    private class featureComparator implements Comparator<String> {
 
         private CodeProfile codeProfile;
 
-        public featureComparator(CodeProfile codeProfile){
+        public featureComparator(CodeProfile codeProfile) {
+
             this.codeProfile = codeProfile;
         }
 
         @Override
         public int compare(String feature1, String feature2) {
+
             double fficf1 = getFfIcf(getFeatureProfile(feature1));
             double fficf2 = getFfIcf(getFeatureProfile(feature2));
-            if(fficf1>fficf2){
+            if (fficf1 > fficf2) {
                 return -1;
-            } else if(fficf1<fficf2){
+            }
+            else if (fficf1 < fficf2) {
                 return 1;
-            } else {
+            }
+            else {
                 return 0;
             }
         }
 
         private Double getFfIcf(FeatureProfile profile) {
+
             Double fficf1;
-            if(profile!=null) {
-                 fficf1 = profile.getFfIcf();
-            } else {
+            if (profile != null) {
+                fficf1 = profile.getFfIcf();
+            }
+            else {
                 fficf1 = 0.0;
             }
             return fficf1;
         }
 
         private FeatureProfile getFeatureProfile(String feature1) {
-            FeatureProfile profile =  null;
-            if(codeProfile.contains(feature1)) {
+
+            FeatureProfile profile = null;
+            if (codeProfile.contains(feature1)) {
                 profile = codeProfile.getProfile(feature1);
             }
             return profile;
@@ -93,32 +113,34 @@ public class RDataFileCreator {
     }
 
     private CodeProfile getCodeProfile(FeatureSpaceAnalyser fsa, Code code, Set<String> features) {
+
         CodeProfile codeProfile2 = null;
-        if(fsa.contains(code)) {
-            codeProfile2= fsa.getAnalysis(code);
+        if (fsa.contains(code)) {
+            codeProfile2 = fsa.getAnalysis(code);
             features.addAll(codeProfile2.getFeatures());
         }
         return codeProfile2;
     }
 
     private void appendScoreLine(CodeProfile codeProfile1, List<String> features, StringBuilder sb) {
+
         Iterator<String> iterator;
         iterator = features.iterator();
         DecimalFormat df = new DecimalFormat("#0.0000");
         int i = 0;
-        while(iterator.hasNext() && i < outputLimit){
+        while (iterator.hasNext() && i < outputLimit) {
             i++;
             String feature = iterator.next();
-            if(codeProfile1 != null && codeProfile1.getFeatures().contains(feature)){
+            if (codeProfile1 != null && codeProfile1.getFeatures().contains(feature)) {
                 double ffIcf = codeProfile1.getProfile(feature).getFfIcf();
                 sb.append(df.format(ffIcf));
-            } else {
+            }
+            else {
                 sb.append(0.0);
             }
-            if(iterator.hasNext() && i < outputLimit)
+            if (iterator.hasNext() && i < outputLimit)
                 sb.append(",");
-            else
-                sb.append("\n");
+            else sb.append("\n");
         }
     }
 }
