ffb31f649f1fde0aea6670b2f0703411195e2b27
Graham Kirby <graham.kirby@st-andrews.ac.uk>
2014-11-18 09:01:43 +0000

    Added GPL licence to record classification module.
    Former-commit-id: 39049e4221a66bc3737b04552f08d20ec0aceaa4

--- record_classification/src/main/java/uk/ac/standrews/cs/digitising_scotland/record_classification/datacleaning/CustomWordCleaner.java
+++ record_classification/src/main/java/uk/ac/standrews/cs/digitising_scotland/record_classification/datacleaning/CustomWordCleaner.java
@@ -1,21 +1,32 @@
+/*
+ * Copyright 2014 Digitising Scotland project:
+ * <http://digitisingscotland.cs.st-andrews.ac.uk/>
+ *
+ * This file is part of the module record_classification.
+ *
+ * record_classification is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * record_classification is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with record_classification. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
 package uk.ac.standrews.cs.digitising_scotland.record_classification.datacleaning;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import uk.ac.standrews.cs.digitising_scotland.record_classification.datastructures.bucket.Bucket;
 import uk.ac.standrews.cs.digitising_scotland.record_classification.datastructures.code.CodeNotValidException;
 import uk.ac.standrews.cs.digitising_scotland.record_classification.datastructures.records.Record;
 import uk.ac.standrews.cs.digitising_scotland.record_classification.exceptions.InputFormatException;
 import uk.ac.standrews.cs.digitising_scotland.util.FileManipulation;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Reads a {@link Bucket} and performs data cleaning such as spelling correction and feature selection on the descriptions in each {@link Record}.
--- record_classification/src/main/scripts/pilot.sh
+++ record_classification/src/main/scripts/pilot.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
+#
+# Copyright 2014 Digitising Scotland project:
+# <http://digitisingscotland.cs.st-andrews.ac.uk/>
+#
+# This file is part of the module record_classification.
+#
+# record_classification is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# record_classification is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with record_classification. If not, see
+# <http://www.gnu.org/licenses/>.
+#
+
 
 # This script will install the record_classification software using 'mvn clean compile' then perform a training/classification run
 # using the PIlot main class. This assumes modern training data and classification file in the pilot study format.
