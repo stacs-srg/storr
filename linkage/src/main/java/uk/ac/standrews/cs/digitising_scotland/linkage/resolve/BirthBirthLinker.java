3ab068c63d1171f2f758f2c95fbd98522d8e8f7c
Alan Dearle <al@st-andrews.ac.uk>
2014-11-24 16:53:27 +0000

    Big set of changes:
    1. Typed reference types added.
    2. Support for type specification from either JSON files or from labelled Java classes.
    3. All integration from last lot of changes now almost complete.
    
    Known bugs:
    1. Some problems with injest of badly formatted json type specifications - needs more checks.
    2. The actual linkage code is not yet working (although the tests do!)
    
    Next steps:
    
    1. Make put in buckets thrown more specific exception.
    2. See if we can put typed values in LXPs rather than strings - investigate interaction with storage and genericity
    3. See if we can have typed put operations.
    
    4. Better management of id space - specifically putting links into Buckets for copies of lxps.
    5. Support for caching of LXPs to support referential integrity.
    6. Support for update.
    7. Transaction support.
    8. Tidy up Javadoc
    9. Some documentation!
    
    
    Former-commit-id: 8f70a67528c96805a42dd877c0193e8488033772

---
+++ linkage/src/main/java/uk/ac/standrews/cs/digitising_scotland/linkage/resolve/BirthBirthLinker.java
@@ -0,0 +1,63 @@
+package uk.ac.standrews.cs.digitising_scotland.linkage.resolve;
+
+
+import uk.ac.standrews.cs.digitising_scotland.jstore.impl.KeyNotFoundException;
+import uk.ac.standrews.cs.digitising_scotland.jstore.impl.LXP;
+import uk.ac.standrews.cs.digitising_scotland.jstore.interfaces.IInputStream;
+import uk.ac.standrews.cs.digitising_scotland.jstore.interfaces.ILXP;
+import uk.ac.standrews.cs.digitising_scotland.jstore.interfaces.IOutputStream;
+import uk.ac.standrews.cs.digitising_scotland.linkage.interfaces.IPair;
+import uk.ac.standrews.cs.digitising_scotland.linkage.lxp_records.Person;
+import uk.ac.standrews.cs.digitising_scotland.linkage.lxp_records.SameAs;
+import uk.ac.standrews.cs.digitising_scotland.linkage.stream_operators.sharder.AbstractPairwiseLinker;
+import uk.ac.standrews.cs.digitising_scotland.util.ErrorHandling;
+
+/**
+ * Created by al on 19/06/2014.
+ * <p/>
+ * Links ILXP records with labels drawn from @link uk.ac.standrews.cs.digitising_scotland.linkage.labels.PersonLabels
+ * Attempts to find birth records with the same person in different roles (e.g. mother-baby and father-baby).
+ */
+public class BirthBirthLinker extends AbstractPairwiseLinker<Person> {
+
+    public BirthBirthLinker(final IInputStream<Person> input, final IOutputStream<IPair<Person>> output) {
+
+        super(input, output);
+    }
+
+    @Override
+    public boolean compare(final IPair<Person> pair) {
+
+        ILXP first = pair.first();
+        ILXP second = pair.second();
+
+        // Return true if we have person in different roles
+
+        try {
+            if ((first.get(Person.ROLE).equals("baby") && second.get(Person.ROLE).equals("mother")) ||
+                    (first.get(Person.ROLE).equals("mother") && second.get(Person.ROLE).equals("baby")) ||
+                    (first.get(Person.ROLE).equals("baby") && second.get(Person.ROLE).equals("father")) ||
+                    (first.get(Person.ROLE).equals("father") && second.get(Person.ROLE).equals("baby"))) {
+                return true;
+            }
+        } catch (KeyNotFoundException e) {
+            ErrorHandling.exceptionError(e, "Key not found");
+        }
+        return false;
+    }
+
+    @Override
+    public void addToResults(final IPair pair, final IOutputStream results) { // TODO these are not typed properly - look at USES OF PAIR
+
+        Person first = (Person) pair.first();  // TODO check dynamic casting
+        Person second = (Person) pair.second();
+
+        // get the people in the right order parent first
+
+        ILXP result_record = new SameAs(first, second, "???", 1.0f);
+
+        results.add(result_record);
+
+
+    }
+}
