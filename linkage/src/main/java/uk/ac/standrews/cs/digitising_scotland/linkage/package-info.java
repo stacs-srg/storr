4080deff7b502b1d3ad57603cbca9bd436ee7b7b
Alan Dearle <al@st-andrews.ac.uk>
2015-01-09 11:58:11 +0000

    separating jstore from digitising scotland
    Former-commit-id: fad51a5e5b837ef60637739d07cb50cad6690dbf

--- linkage/src/main/java/uk/ac/standrews/cs/digitising_scotland/linkage/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * This package implements linkage of population records.
- */
-package uk.ac.standrews.cs.digitising_scotland.linkage;
--- record_classification/src/main/resources/scripts/checkScript.sh
+++
@@ -1,1 +0,0 @@
-type $1 >/dev/null 2>&1 || { echo $1 "required but it's not installed.  Aborting."; exit 1; }
