ace41142807c85cc806b2f250ea642914523ab97
Simone Conte <sic2@st-andrews.ac.uk>
2016-09-26 17:04:48 +0100

    added storr-rest and storr-jetty as modules to the project. Implementation is work in progress
    Former-commit-id: 5f3bc3cc24953634a065e65ca05098199d244c4d

---
+++ storr-rest/src/main/java/uk/ac/standrews/cs/storr/RESTConfig.java
@@ -0,0 +1,18 @@
+package uk.ac.standrews.cs.storr;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import uk.ac.standrews.cs.storr.json.JacksonProvider;
+
+/**
+ * @author Simone I. Conte "sic2@st-andrews.ac.uk"
+ */
+public class RESTConfig {
+
+    private final static String REST_PACKAGE = "uk.ac.standrews.cs.storr.rest";
+
+    public ResourceConfig build() {
+        return new ResourceConfig()
+                .packages(REST_PACKAGE)
+                .register(JacksonProvider.class);
+    }
+}
