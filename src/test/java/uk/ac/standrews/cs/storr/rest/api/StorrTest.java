2b1ee30b04c4f11278d1c03f0bdf72ce53bded14
sic2 <sic2@st-andrews.ac.uk>
2017-05-23 18:10:49 +0100

    Issue #3 is solved and tested

--- src/test/java/uk/ac/standrews/cs/storr/rest/api/StorrTest.java
+++ src/test/java/uk/ac/standrews/cs/storr/rest/api/StorrTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 Systems Research Group, University of St Andrews:
+ * <https://github.com/stacs-srg>
+ *
+ * This file is part of the module storr.
+ *
+ * storr is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * storr is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with storr. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
 package uk.ac.standrews.cs.storr.rest.api;
 
 import org.glassfish.jersey.test.JerseyTest;
@@ -40,13 +56,101 @@ public class StorrTest extends JerseyTest {
     @Test
     public void createBasicILXP() {
 
-        String data = "{\n" +
-                "\t\"number\" : 1\n" +
+        String data = "{" +
+                "\"number\" : 1" +
                 "}";
 
+        createILXPFromJSON(data);
+    }
+
+    @Test
+    public void createILXPFromJSON_1() {
+
+        String data = "{" +
+                "    \"glossary\": {" +
+                "        \"title\": \"test glossary\"," +
+                "        \"GlossDiv\": {" +
+                "            \"title\": \"S\"," +
+                "            \"valid\": true" +
+                "        }" +
+                "    }" +
+                "}";
+
+        createILXPFromJSON(data);
+    }
+
+    @Test
+    public void createILXPFromJSON_2() {
+
+        String data = "{" +
+                "\"name\": \"John\"," +
+                "\"age\": 30," +
+                "\"cars\": [\"Ford\", \"BMW\", \"Fiat\", \"Ferrari\"]" +
+                "}";
+
+        createILXPFromJSON(data);
+    }
+
+    @Test
+    public void createILXPFromJSON_3() {
+
+        String data = "{" +
+                "   \"glossary\": [{" +
+                "    \"title\": \"test title\"" +
+                "}]" +
+                "}";
+
+        createILXPFromJSON(data);
+    }
+
+    @Test
+    public void createILXPFromJSON_4() {
+
+        String data = "{" +
+                "  \"problems\": [{" +
+                "    \"Diabetes\": [{" +
+                "      \"medications\": [{" +
+                "        \"medicationsClasses\": [{" +
+                "          \"className\": [{" +
+                "            \"associatedDrug\": [{" +
+                "              \"name\": \"asprin\"," +
+                "              \"dose\": \"\"," +
+                "              \"strength\": \"500 mg\"" +
+                "            }]," +
+                "            \"associatedDrug#2\": [{" +
+                "              \"name\": \"somethingElse\"," +
+                "              \"dose\": \"\"," +
+                "              \"strength\": \"500 mg\"" +
+                "            }]" +
+                "          }]," +
+                "          \"className2\": [{" +
+                "            \"associatedDrug\": [{" +
+                "              \"name\": \"asprin\"," +
+                "              \"dose\": \"\"," +
+                "              \"strength\": \"500 mg\"" +
+                "            }]," +
+                "            \"associatedDrug#2\": [{" +
+                "              \"name\": \"somethingElse\"," +
+                "              \"dose\": \"\"," +
+                "              \"strength\": \"500 mg\"" +
+                "            }]" +
+                "          }]" +
+                "        }]" +
+                "      }]," +
+                "      \"labs\": [{" +
+                "        \"missing_field\": \"missing_value\"" +
+                "      }]" +
+                "    }]" +
+                "  }]" +
+                "}";
+
+        createILXPFromJSON(data);
+    }
+
+    private void createILXPFromJSON(String JSON) {
         Response response = target("/REPO/BUCKET")
                 .request()
-                .post(Entity.json(data));
+                .post(Entity.json(JSON));
 
         assertEquals(response.getStatus(), HTTPStatus.CREATED);
 
@@ -57,5 +161,6 @@ public class StorrTest extends JerseyTest {
         assertEquals(ILXPHandle[1], "REPO");
         assertEquals(ILXPHandle[2], "BUCKET");
         assertFalse(ILXPHandle[3].isEmpty());
+
     }
 }
